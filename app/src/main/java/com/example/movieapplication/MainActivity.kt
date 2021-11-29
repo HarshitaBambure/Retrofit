d541deeea4f6f38d76a5f83d7c7e753d0879bff9
Harshita Bambure <harshitabambure@gmail.com>
2021-11-29 12:02:42 +0530

    basic ui

--- app/src/main/java/com/example/movieapplication/MainActivity.kt
+++ app/src/main/java/com/example/movieapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.movieapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ToggleButton
+import com.example.movieapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+      binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+
+
     }
 }
